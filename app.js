4b0a92924079d6306cbaa391427e2d433c6e60d3
gise2023 <gogiselleaojeda@gmail.com>
2022-12-02 23:33:36 -0300

    Mi primer commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3030;
+const path = require('path');
+
+app.use(express.static(path.resolve(__dirname,'public')));
+
+app.get('/',(req,res) => res.sendFile(path.resolve(__dirname,'views','home.html')))
+
+app.listen(port, () => console.log(`Servidor corriendo en http://localhost:${port}`))
+
